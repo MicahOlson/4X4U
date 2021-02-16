0c3b7fd94b48e6426700104e7c7745942e85e4dd
Jon Stump <jmstump@gmail.com>
2021-02-16 11:37:57 -0800

    add standard API call
    Co-authored-by: Olha Hizhytska <olgainfotech@gmail.com>
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -6,24 +6,20 @@ import Arbitrage from './services/standard-exchange-api.js';
 
 $(document).ready(function() { 
   // Arbitrage
-  let exchanges = {};
   $('#test').click(function(event) {
     event.preventDefault();
     let topTen = ['USD', 'EUR', 'JPY']; // , 'GBP', 'AUD', 'CAD', 'CHF', 'CNY', 'SEK', 'NZD'
-    // let userSelection = "USD" // base value from HTML;
+    // let userCurrency = "USD" // base value from HTML;
+    // let userAmount = 15 // amount value from HTML;
     topTen.forEach(function(base) {
       if (!sessionStorage[base]) {
         let promise = Arbitrage.getArbitrage(base);
         promise.then(function(response) {
           const baseRates = JSON.parse(response);
-          exchanges[base] = baseRates;
           sessionStorage.setItem(base, JSON.stringify(baseRates));
         }, function(error) {
           console.log(`API CALL ERROR -> ${error}`);
         });
-      } else {
-        console.log("Else conditional reached.");
-        exchanges[base] = JSON.parse(sessionStorage.getItem(base));
       }
     });
   });
--- src/services/standard-exchange-api.js
+++ src/services/standard-exchange-api.js
@@ -2,14 +2,14 @@ export default class Arbitrage {
   static getArbitrage(baseCurrency) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCurrency}`
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCurrency}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
         } else {
           reject(request.response);
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
     });
