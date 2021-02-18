b9dedb446029b868f2526e8aa64da03a47fdc7cc
Jon Stump <jmstump@gmail.com>
2021-02-18 11:05:55 -0800

    final tweaks on page ordering
    Co-authored-by: Jo Miller <joannadawnmiller@gmail.com>
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
    Co-authored-by: Olha Hizhytska <olgainfotech@gmail.com>
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>

--- src/main.js
+++ src/main.js
@@ -7,7 +7,27 @@ import { selectCurrencies } from './js/currency-list.js';
 import { findArbitrage, getExchangeRates } from './js/arbitrage.js';
 import { demoRates } from './js/demo-rates.js';
 
-
+function celebrate() {
+  var duration = 15 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+  
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+  var interval = setInterval(function() {
+    var timeLeft = animationEnd - Date.now();
+  
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+  
+    var particleCount = 50 * (timeLeft / duration);
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+  }, 250);
+}
 
 $(document).ready(function() {
   function errorCheck(response) {
@@ -56,6 +76,7 @@ $(document).ready(function() {
   });
 
   $('#runDemo').click(function(event) {
+    celebrate();
     event.preventDefault();
     const userCurrency = "USD";
     const arbitrageResults = findArbitrage(userCurrency, demoRates);
